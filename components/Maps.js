2e30f05c2918c383ac903b9c382cd6c715684553
kimwoobin <kwb020312@naver.com>
2020-09-23 18:53:01 +0900

    맵 위치 구현

--- components/Maps.js
+++ components/Maps.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import * as API from "../api/Works";
+import { Text } from "react-native";
 
 function Maps({ address }) {
   const [location, setLocation] = useState({
@@ -8,35 +9,49 @@ function Maps({ address }) {
     longitude: 0,
   });
 
-  useEffect(() => {
-    const getPoint = async () => {
-      const points = await API.findAddress(address);
-      setLocation({
-        latitude: points.documents[0].y,
-        longitude: points.documents[0].x,
-      });
-    };
+  const [isLoading, setIsLoading] = useState(true);
 
+  const getPoint = async () => {
+    const points = await API.findAddress(address);
+    setLocation({
+      latitude: Number(points.documents[0].y),
+      longitude: Number(points.documents[0].x),
+    });
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    // useEffect 사용하여 초깃값으로 설정하지 않는경우 맵 위치가 고정되어 바뀌지 않음
+    setLocation({
+      latitude: 0,
+      longitude: 0,
+    });
     getPoint();
   }, []);
 
   return (
-    <MapView
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: 0.009,
-        longitudeDelta: 0.004,
-      }}
-    >
-      <MapView.Marker
-        coordinate={{
-          latitude: location.latitude,
-          longitude: location.longitude,
-        }}
-      />
-    </MapView>
+    <>
+      {isLoading === true ? (
+        <Text>Loaded Data Map...</Text>
+      ) : (
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.009,
+            longitudeDelta: 0.004,
+          }}
+        >
+          <MapView.Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        </MapView>
+      )}
+    </>
   );
 }
 
